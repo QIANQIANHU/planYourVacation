d88da2aacd3603707eb0da2e414055308d991f36
Ainur Dzhaianbaeva and Qianqian Hu <ainur@>
2018-04-04 16:42:55 -0700

    add clickable function to wiki info in places details

--- src/app/place.model.ts
+++ src/app/place.model.ts
@@ -1,3 +1,3 @@
 export class Place {
-  constructor(public ImageUrl: string, public wikiUrl: string, public name: string, public description: string) { }
+  constructor(public imageUrl: string, public wikiUrl: string, public name: string, public description: string) { }
 }
